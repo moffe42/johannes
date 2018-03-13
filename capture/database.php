b42f0cc94c611de73b7025e84d74e6e58f39d243
Jacob Christiansen <jacob@colourbox.com>
2018-03-13 22:46:31 +0100

    Moved db stuff to seperate file

---
+++ capture/database.php
@@ -0,0 +1,12 @@
+<?php
+require_once('../config/config.php');
+
+try {
+    // Initialize DB
+    $dbh = new PDO($config['db.dsn'], $config['db.user'], $config['db.pass'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"));
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (Exception $e) {
+    exit("Connection to database failed. Please contact admin");
+}
+
--- capture/get_capture_codes.php
+++ capture/get_capture_codes.php
@@ -3,15 +3,7 @@ error_reporting(-1);
 
 require_once('../config/config.php');
 require_once('validate.php');
-
-try {
-    // Initialize DB
-    $dbh = new PDO($config['db.dsn'], $config['db.user'], $config['db.pass'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"));
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch (Exception $e) {
-    exit("Connection to database failed. Please contact admin");
-}
+require_once('database.php');
 
 $stmt = $dbh->prepare('SELECT * FROM scout');
 $stmt->execute();
