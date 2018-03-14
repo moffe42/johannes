b48bac8bc8159b9ee0d14dc71a1859d15c385442
Jacob Christiansen <jacob@colourbox.com>
2018-03-14 14:58:13 +0100

    Made it pretty

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,66 @@
 <html>
-	
+	<head>
+		<title>Apokalypseløbet 2015 - APOKASTATUS</title>
+		<meta charset="utf-8">
+		<meta http-equiv="refresh" content="30" >
+		<style type="text/css">
+			body {
+				font-family: "Droid", sans-serif;
+				background-image: url( http://www.apokalypse.dk/wp-content/uploads/2012/06/topographic1.jpg ) !important;
+				background-repeat: repeat !important;
+				background-position: top left !important;
+				color: #FFF;
+			}
+			table.content{
+				background-color: #000;
+				margin-left: auto;
+				margin-right: auto;
+				border: 1px solid #FFF;
+				border-collapse: collapse;
+				width: 99%;
+			}
+			table.content td, table .content th {
+				border: 1px solid #FFF;
+				padding: 5px;
+			}
+			table#objectcount td:nth-child(2), table#capturecount td:nth-child(2) {
+				text-align: center;
+			}
+			table.container {
+				width: 100%;
+			}
+			table.container td {
+				vertical-align: top;
+				padding-left: 5px;
+				padding-right: 5px;
+			}
+			table.container>td:nth-child(1) {
+				width: 50%;
+			}
+			table.container>td:nth-child(2), table.container>td:nth-child(3) {
+				width: 25%;
+			}
+			a:visited {
+    				color: #FFF;
+			}
+			a:hover {
+				 font-weight: bold;
+	  		}
+			.headline {
+				text-align: center;
+			}
+			.smalltext {
+				font-size: 10px;
+				color: #555;
+			}
+		</style>
+	</head>
 	<body>
-		<h1>ADMIN</h1>
+		<div class="headline">
+			<img src="http://www.apokalypse.dk/wp-content/uploads/2012/03/apokalypse_logo2.png" alt="Apokalypseløbet – Danmarks mest udfordrende spejderløb">
+			<h1>ADMIN</h1>
+		</div>
+		<h1>Code generatio</h1>
 		<p>Add ?gogo=yes to the requested URL for the action to take affect. This is to prevent accidents.</p>
 		<ul>
 			<li><a href="genpass.php">Generate passwords for all sjaks</a></li>
@@ -13,7 +72,7 @@
 		<h1>Status</h1>
 		<ul>
 			<li><a href="../status">Løbsstatus</a></li>
-			<li><a href="../sjakstatus">Sjak status (add /<sjakid>)</a></li>
+			<li><a href="../sjakstatus">Sjak status (add /sjakid)</a></li>
 		</ul>
 	</body>
 </html>
