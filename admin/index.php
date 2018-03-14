cc1096ab143fa24014efabd8ba8568e48d692d56
Jacob Christiansen <jacob@colourbox.com>
2018-03-14 14:50:43 +0100

    Added status to admin page

--- admin/index.php
+++ admin/index.php
@@ -2,10 +2,18 @@
 	
 	<body>
 		<h1>ADMIN</h1>
+		<p>Add ?gogo=yes to the requested URL for the action to take affect. This is to prevent accidents.</p>
 		<ul>
-			<li><a href="gen_pass.php">Generate passwords for all sjaks (add ?gogo=yes to update the passwords)</a></li>
+			<li><a href="genpass.php">Generate passwords for all sjaks</a></li>
 			<li><a href="gen_capture_codes.php?cc=40">Generate 40 capture codes for each scout</a></li>
 			<li><a href="gen_object_codes.php?cc=40">Generate 40 object codes for each object</a></li>
+			<li><a href="reset.php">RESET ALL CAPTURES AND OBJECT LOGS</a></li>
+		</ul>
+
+		<h1>Status</h1>
+		<ul>
+			<li><a href="../status">Løbsstatus</a></li>
+			<li><a href="../sjakstatus">Sjak status (add /<sjakid>)</a></li>
 		</ul>
 	</body>
 </html>
